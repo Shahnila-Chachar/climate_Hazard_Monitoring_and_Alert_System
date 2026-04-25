c14bc6e26e0c8db7c0c0befac41020f4246927b4
TaibaFatima006 <taibafatima.bscsf25@iba-suk.edu.pk>
2026-04-25 15:48:37 +0500

    rescue plane and alert services added

--- service/AlertServices.java
+++ service/AlertServices.java
@@ -1,5 +1,29 @@
 package service;
 
-public class AlertServices {
+    class AlertServices extends HazardResponse{
+	AlertServices(String hazardType){
+		super(hazardType);
+	}
 
+	@Override
+	public String generateAlert(){
+         
+        String type = getHazardType(); 
+		if(type.equalsIgnoreCase("flood"))
+			return "Alert: Flood Risk Detected!";
+		else if(type.equalsIgnoreCase("fire"))
+			return "Alert: Fire Emergency";
+		else if(type.equalsIgnoreCase("	EarthQuake"))
+			return "Alert: EarthQuake";
+		else if (type.equalsIgnoreCase("Heatwave"))
+			return "Alert: Heatwave Risk";
+		else
+			return "No hazard detected";
+	}
+
+	@Override
+	public String generateRescuePlan(){
+		return "Handled by the rescue service team";
+	}
 }
+
